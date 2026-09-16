b0524b51eb50073f0bcd944b589116fcf0412534
vegaquake <vegaquake@foxPad>
2026-09-16 15:45:38 -0500

    Added comments to describe functions

--- src/main/java/edu/umn/d/cs1632/Main.java
+++ src/main/java/edu/umn/d/cs1632/Main.java
@@ -1,15 +1,18 @@
 package edu.umn.d.cs1632;
 
-
 import java.io.IOException;
 import com.opencsv.exceptions.CsvException;
 
 public class Main {
 		
     public static void main(String[] args) throws IOException, CsvException {
-       
+       // Create new session for CSVFunctions
        CSVFunctions session = new CSVFunctions();
+       
+       // Start by initializing the CSV file
        session.read();
+       
+       // Begin query
        session.query();
     }
 }
